1e34aaf9a6cdc5065005c8e3536eb3e558f14df4
donkorBN <projectmanuel89@gmail.com>
2023-06-20 19:09:44 -0700

    Update of Prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 *
-	 * Code is written by donkor
-	 *
-	 * Return: 0
-	 */
 
+/**
+ * main - print _putchar
+ *
+ * This function prints the string "_putchar" to the standard output stream.
+ *
+ * Return: 0
+ */
 
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+int main(void)
+{
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+  return (0);
+}
