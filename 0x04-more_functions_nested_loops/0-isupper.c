73296fce4e8f97aded55e79fec6930c2f8468f6c
donkorBN <projectmanuel89@gmail.com>
2023-06-23 14:47:08 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+*_isupper-checksforuppercasecharacter
+*@c: The character to be checked.
+*Return: Always0.
+*/
+int_isupper(int c)
+{
+
+
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
