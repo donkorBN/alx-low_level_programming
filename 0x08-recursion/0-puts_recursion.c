4d5dcfa990037c4da30779bb8969273fb28815a2
donkorBN <projectmanuel89@gmail.com>
2023-07-05 12:21:28 -0700

    Prints a string, then a newline

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - a function like puts();
+ * @a: input
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *a)
+{
+	if (*a)
+	{
+		_putchar(*a);
+		_puts_recursion(a + 1);
+	}
+
+	else
+		_putchar('\n');
+}
